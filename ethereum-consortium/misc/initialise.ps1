f0f820736f0a4bb9c1dd66e002b0a52936f65dac
Mike Ormond <mormond@microsoft.com>
2017-02-07 16:52:24 +0000

    Add initialisation ps script

---
+++ ethereum-consortium/misc/initialise.ps1
@@ -0,0 +1,12 @@
+login-azurermaccount 
+
+Select-AzureRmSubscription -Subscription Name "FounderAscendPlus"
+
+$RgConsortiumName = "Latest2"
+New-AzureRmResourceGroup -Location "westeurope" -Name $RgConsortiumName
+New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortium.json -TemplateParameterFile .\misc\template.consortium.params.json -ResourceGroupName $RgConsortiumName
+
+#$RgMemberName = ""
+#New-AzureRmResourceGroup -Location "westeurope" -Name $RgMemberName
+#New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.json -TemplateParameterFile .\misc\template.consortium.params.participant1.json -ResourceGroupName $RgMemberName
+
