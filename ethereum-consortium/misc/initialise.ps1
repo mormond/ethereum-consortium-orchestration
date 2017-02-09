f0e76783d81687711493966f4c71c49b8bfe8810
Mike Ormond <mormond@microsoft.com>
2017-02-09 13:33:09 +0000

    More script params tweaks

--- ethereum-consortium/misc/initialise.ps1
+++ ethereum-consortium/misc/initialise.ps1
@@ -1,19 +1,20 @@
 login-azurermaccount 
 
-$RgConsortiumName = "Latest2"
+$RgConsortiumName = "BC_Founder"
 
 New-AzureRmResourceGroup -Location "westeurope" -Name $RgConsortiumName
 New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortium.json `
  -TemplateParameterFile .\misc\template.consortium.params.json `
  -ResourceGroupName $RgConsortiumName
 
-$RgMemberName = "Latest22" #Participant resource group -Name
+$RgMemberName = "BC_Member1" #Participant resource group name
+$MemberName = "member1" #Name that will show up in dashboard
 $DashboardIp = "" #IP of the consortium dashboard node (which is also the registrar node)
 
 New-AzureRmResourceGroup -Location "westeurope" -Name $RgMemberName
 New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.json `
  -TemplateParameterFile .\misc\template.consortium.params.participant1.json `
  -ResourceGroupName $RgMemberName `
- -consortiumMemberName $RgMemberName `
+ -consortiumMemberName $MemberName `
  -dashboardIp $DashboardIp `
  -registrarIp $DashboardIp
