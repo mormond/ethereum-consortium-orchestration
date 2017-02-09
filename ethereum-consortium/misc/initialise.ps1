5045d96308ec4d4eafa0c91ea1f096a8a0f89b90
Mike Ormond <mormond@microsoft.com>
2017-02-09 12:09:22 +0000

    Updated powershell deployment script

--- ethereum-consortium/misc/initialise.ps1
+++ ethereum-consortium/misc/initialise.ps1
@@ -1,10 +1,19 @@
 login-azurermaccount 
 
 $RgConsortiumName = "Latest2"
+
 New-AzureRmResourceGroup -Location "westeurope" -Name $RgConsortiumName
-New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortium.json -TemplateParameterFile .\misc\template.consortium.params.json -ResourceGroupName $RgConsortiumName
+New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortium.json `
+ -TemplateParameterFile .\misc\template.consortium.params.json `
+ -ResourceGroupName $RgConsortiumName
 
-#$RgMemberName = ""
-#New-AzureRmResourceGroup -Location "westeurope" -Name $RgMemberName
-#New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.json -TemplateParameterFile .\misc\template.consortium.params.participant1.json -ResourceGroupName $RgMemberName
+$RgMemberName = "Latest22" #Participant resource group -Name
+$DashboardIp = "" #IP of the consortium dashboard node (which is also the registrar node)
 
+New-AzureRmResourceGroup -Location "westeurope" -Name $RgMemberName
+New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.json `
+ -TemplateParameterFile .\misc\template.consortium.params.participant1.json `
+ -ResourceGroupName $RgMemberName `
+ -consortiumMemberName $RgMemberName `
+ -dashboardIp $DashboardIp `
+ -registrarIp $DashboardIp
