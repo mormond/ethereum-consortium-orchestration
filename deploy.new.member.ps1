a002e3e6cf1f1dc4e718f570672bdb55fb394143
Mike Ormond <mormond@microsoft.com>
2017-02-16 15:51:11 +0000

    Update deploy new member

--- deploy.new.member.ps1
+++ deploy.new.member.ps1
@@ -1,3 +1,31 @@
+#Run from the etherem-consortium folder
+
+$rgName = "test1233"
+$location = "westeurope"
+$subName = "FounderAscendPlus"
+
+$hostingPlanName = "AppServicesHostingPlan"
+$skuName = "S1"
+$administratorLogin = "dbadmin"
+$databaseName = "accounts"
+
+function CheckAndAuthenticateIfRequired {
+    Try {
+        $a = Get-AzureRmContext
+    }
+    Catch {
+        login-azurermaccount 
+    }
+}
+
+Write-Host "Logging into Azure"
+CheckAndAuthenticateIfRequired
+
+Write-Host "Setting subscription to: $subName"
+Select-AzureRmSubscription -SubscriptionName $subName
+
+Write-Host "Creating new resource group: $rgName"
+New-AzureRmResourceGroup -Location $location -Name $rgName
 
 $RgMemberName = "BC_Member1" #Participant resource group name
 $MemberName = "member1" #Name that will show up in dashboard
@@ -10,3 +38,26 @@ New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.jso
  -consortiumMemberName $MemberName `
  -dashboardIp $DashboardIp `
  -registrarIp $DashboardIp
+
+  #
+ # Add the App Service components (web site + SQL Server)
+ #
+ #
+
+Write-Host "Deploying web site / API components."
+New-AzureRmResourceGroupDeployment -TemplateFile ".\node-interface-components\template.web.components.json" `
+  -ResourceGroupName $rgName `
+  -hostingPlanName $hostingPlanName `
+  -skuName $skuName `
+  -administratorLogin $administratorLogin `
+  -databaseName $databaseName 
+
+#
+# Add the VNET Integration
+#
+#
+
+Write-Host "Adding VNET integration."
+$invocationPath = Split-Path $MyInvocation.MyCommand.Path
+
+& ($invocationPath + "\node-interface-components\app.service.vnet.integration.ps1") 
