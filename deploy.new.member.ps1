67e5c3e6de7b8161fe3d26aadd967978c4141f2c
Mike Ormond <mormond@microsoft.com>
2017-02-17 08:59:02 +0000

    Fix new member script

--- deploy.new.member.ps1
+++ deploy.new.member.ps1
@@ -1,13 +1,22 @@
-#Run from the etherem-consortium folder
+#
+# Script to deploy an additional member to an existing consortium
+# The address of the network bootnode must be provided
+#
 
-$rgName = "test1233"
-$location = "westeurope"
-$subName = "FounderAscendPlus"
-
-$hostingPlanName = "AppServicesHostingPlan"
-$skuName = "S1"
-$administratorLogin = "dbadmin"
-$databaseName = "accounts"
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$rgMemberName, #Participant resource group name
+    [string]$memberName, #Name that will show up in dashboard
+    [string]$dashboardIp, #IP of the consortium dashboard node (which is also the registrar node)
+    [string]$location = "WestEurope",
+    [string]$subName = "FounderAscendPlus",
+    [string]$sqlAdminLogin = "dbadmin",
+    [string]$databaseName = "accounts",
+    [Parameter(Mandatory=$True)]
+    [securestring]$sqlAdminPassword,
+    [string]$hostingPlanName = "AppServicesHostingPlan",
+    [string]$skuName = "S1"
+)
 
 function CheckAndAuthenticateIfRequired {
     Try {
@@ -27,29 +36,26 @@ Select-AzureRmSubscription -SubscriptionName $subName
 Write-Host "Creating new resource group: $rgName"
 New-AzureRmResourceGroup -Location $location -Name $rgName
 
-$RgMemberName = "BC_Member1" #Participant resource group name
-$MemberName = "member1" #Name that will show up in dashboard
-$DashboardIp = "" #IP of the consortium dashboard node (which is also the registrar node)
-
-New-AzureRmResourceGroup -Location "westeurope" -Name $RgMemberName
+New-AzureRmResourceGroup -Location "westeurope" -Name $rgMemberName
 New-AzureRmResourceGroupDeployment -TemplateFile .\template.consortiumMember.json `
  -TemplateParameterFile .\misc\template.consortium.params.participant1.json `
- -ResourceGroupName $RgMemberName `
- -consortiumMemberName $MemberName `
- -dashboardIp $DashboardIp `
- -registrarIp $DashboardIp
+ -ResourceGroupName $rgMemberName `
+ -consortiumMemberName $memberName `
+ -dashboardIp $dashboardIp `
+ -registrarIp $dashboardIp
 
-  #
- # Add the App Service components (web site + SQL Server)
- #
- #
+#
+# Add the App Service components (web site + SQL Server)
+#
+#
 
 Write-Host "Deploying web site / API components."
-New-AzureRmResourceGroupDeployment -TemplateFile ".\node-interface-components\template.web.components.json" `
+$webOutputs = New-AzureRmResourceGroupDeployment -TemplateFile ".\node-interface-components\template.web.components.json" `
   -ResourceGroupName $rgName `
   -hostingPlanName $hostingPlanName `
   -skuName $skuName `
-  -administratorLogin $administratorLogin `
+  -administratorLogin $sqlAdminLogin `
+  -administratorLoginPassword $sqlAdminPassword `
   -databaseName $databaseName 
 
 #
@@ -58,6 +64,9 @@ New-AzureRmResourceGroupDeployment -TemplateFile ".\node-interface-components\te
 #
 
 Write-Host "Adding VNET integration."
+
 $invocationPath = Split-Path $MyInvocation.MyCommand.Path
 
-& ($invocationPath + "\node-interface-components\app.service.vnet.integration.ps1") 
+& ($invocationPath + "\node-interface-components\app.service.vnet.integration.ps1") `
+    -rgName $rgName `
+    -appName $webOutputs.Outputs.webApiName.Value
